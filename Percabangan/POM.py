7d92ed7b5d68c528db1075f517cfda751122854d
putrinofita <putrinofitasari65@gmail.com>
2025-05-23 05:26:52 +0700

    Update POM.py

--- Percabangan/POM.py
+++ Percabangan/POM.py
@@ -10,34 +10,72 @@
 # jadi , jika user memilih type pertalite maka pada variable harga berisi ( liter * pertalite ) dan seterusnya , 
 # lalu pada akhir program mnculkan harga yg harus dibayarkan sesuai berapa banyak liter yg akan di beli sesuai harga type bahaan bakar tersebut
 
-
+print ("*" * 80)
 print ("Selamat datang di POM MINI tata")
 print ("Kami Menyediakan (pertamax , pertalite , dexlite , solar dan pertamax turbo)")
+print ("*" * 80)
+
+# list harga
+pertamax = 12400
+pertalite = 10000
+dexlite = 13350
+solar = 6800
+pertamax_turbo = 13300
 
 bahanBakar = input("Pilih bahan bakar anda: ").lower()
+# .lower() berfungsi agar apa saja yang diinputkan oleh user berubah menjadi huruf kecil semua
+# contoh: yang user input "PeRtAmaX" yang akan di terima oleh program "pertamax"
+
 if bahanBakar == "pertamax" or bahanBakar == "pertalite" or bahanBakar == "dexlite" or bahanBakar == "solar" or bahanBakar == "pertamax turbo" :
+# kenapa ada percabangan untuk mengecek ini?? 
+# Ini berfungsi agar ketika user tidak menginputkan bahan bakar dengan benar program akan berhenti berjalan
+
     banyak = float(input("Berapa liter yang akan anda beli: "))
+    # jika bahan bakar sudah diinputkan dengan benar maka program akan lanjut menanyakan berapa liter yang akan dibeli lebih user
 
     if bahanBakar == "pertamax":
-        total = banyak * 12_000
+        total = banyak * pertamax
     elif bahanBakar == "pertalite":
-        total = banyak * 14_000
+        total = banyak * pertalite
     elif bahanBakar == "dexlite":
-        total = banyak * 16_000
+        total = banyak * dexlite
     elif bahanBakar == "solar":
-        total = banyak * 18_000
+        total = banyak * solar
+    elif bahanBakar == "pertamax turbo":
+        total = banyak * pertamax_turbo
+    # kenapa banyak dikali sama pertamax/pertalite/dexlite/solar/pertamax_turbo? kenapa bukan sama angka(harganya)?
+    # coba lihat di baris 18 sampai 23 disitu aku nyantumin list harga 
+    # nahh bagian itu berfungsi agar ketika ada kenaikan harga kita hanya mengubah disana saja
+    # tanpa mengotak-atik code yang ada di dalam percabangan ini
     else:
-        total = banyak * 19_000
+        print ("bahan bakar invalid, masukkan bahan bakar dengan benar")
+
+    print ("*" * 80)
+    # abaikan bagian yang seperti ini
 
     total= int(total)
+    # aku buat konversi ini karena di baris 33 sampai 45 itu float jadi agar tidak ada koma di belakang harga jadi konversi dulu
+    # jika kamu kepo jadikan baris 56 menjadi komen (dengan cara ctrl /) saja dan coba run
     
     print ("Total yang harus anda bayarkan Rp.", total)
 
     bayar = int(input("Inputkan nominal uang yang akan anda bayarkan: "))
 
-    kembalian = total - bayar
+    if bayar > total:
+        kembalian = bayar - total
+        print ("Jadi, kembalian yang anda dapatkan sebanyak Rp.", kembalian)
+    elif bayar == total:
+        print ("Terima kasih, Uang anda pas")
+    elif bayar < total:
+        kembalian = total - bayar
+        print ("Uang anda kurang sebanyak Rp.", kembalian)
+    else:
+        print ("nominal yang anda inputkan kurang tepat")
 
-    print ("Jadi, kembalian yang anda dapatkan sebanyak Rp.", kembalian)
-
+    # dan kenapa bagian bayar ada di if?
+    # aku taro di if agar tidak ada traceback
+    # info tentang traceback ada di google
 else :
     print ("Bahan bakar yang anda inputkan tidak ada")
+
+print ("*" * 80)
